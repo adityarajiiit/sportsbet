fa89d6d98274ef61d8108b16492812a592c365e6
Aditya Raj <aditya0405raj@gmail.com>
2025-07-20 01:53:02 +0530

    new commit

--- app/(site)/dashboard/page.jsx
+++ app/(site)/dashboard/page.jsx
@@ -6,20 +6,16 @@ const dashboard=()=>{
     if(session.status==="loading"){
         return <div>Loading...</div>
     }
-    let url;
-    if(process.env.DOMAIN=='localhost'){
-        url=`http://${process.env.DOMAIN}:3000`
-    }
-    else{
-        url=`http://${process.env.DOMAIN}`
-    }
+
     return(
         <div>
             <h1>Dashboard</h1>
             <p>Status: {session.status}</p>
             
             <p>Hi {session?.data?.user?.email}</p>
-            {session?.data?.user?.email && <button onClick={()=>signOut({callbackUrl:url})}>Sign Out</button>}
+            {session?.data?.user?.email && <button onClick={()=>{
+                signOut({callbackUrl:'/'})
+            }}>Sign Out</button>}
         </div>
     )
 }
